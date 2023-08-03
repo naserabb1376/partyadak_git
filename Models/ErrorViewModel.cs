23c4f0e347625c38e01d236ab1b5016bff3cddd1
Naser <Naserabb76@outlook.com>
2023-08-03 20:31:41 +0330

    first_commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace auto_part_yadak.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
